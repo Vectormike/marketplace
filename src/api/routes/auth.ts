42cab7e2a8866f28fae325d80fcafc307398be0a
Redeemer40 <victorjonah199@gmail.com>
2020-06-11 18:21:48 +0100

    route for signin

--- src/api/routes/auth.ts
+++ src/api/routes/auth.ts
@@ -1,36 +1,23 @@
-import { Router, Request, Response, NextFunction } from 'express';
-import { Container } from 'typedi';
-import AuthService from '../../services/auth';
-import { IUserInputDTO } from '../../interfaces/IUser';
-import middlewares from '../middlewares';
+import { Router } from 'express';
+import AuthController from '../controllers/auth';
+//import middlewares from '../middlewares';
 import { celebrate, Joi } from 'celebrate';
 
 const route = Router();
 
 export default (app: Router) => {
   app.use('/auth', route);
-
   route.post(
     '/signup',
     celebrate({
       body: Joi.object({
         name: Joi.string().required(),
         email: Joi.string().required(),
+        phoneNumber: Joi.number().required(),
         password: Joi.string().required(),
       }),
     }),
-    async (req: Request, res: Response, next: NextFunction) => {
-      const logger = Container.get('logger');
-      logger.debug('Calling Sign-Up endpoint with body: %o', req.body )
-      try {
-        const authServiceInstance = Container.get(AuthService);
-        const { user, token } = await authServiceInstance.SignUp(req.body as IUserInputDTO);
-        return res.status(201).json({ user, token });
-      } catch (e) {
-        logger.error('🔥 error: %o', e);
-        return next(e);
-      }
-    },
+    AuthController.registerUser,
   );
 
   route.post(
@@ -41,19 +28,7 @@ export default (app: Router) => {
         password: Joi.string().required(),
       }),
     }),
-    async (req: Request, res: Response, next: NextFunction) => {
-      const logger = Container.get('logger');
-      logger.debug('Calling Sign-In endpoint with body: %o', req.body)
-      try {
-        const { email, password } = req.body;
-        const authServiceInstance = Container.get(AuthService);
-        const { user, token } = await authServiceInstance.SignIn(email, password);
-        return res.json({ user, token }).status(200);
-      } catch (e) {
-        logger.error('🔥 error: %o',  e );
-        return next(e);
-      }
-    },
+    AuthController.loginUser,
   );
 
   /**
@@ -65,15 +40,15 @@ export default (app: Router) => {
    * emitted for the session and add it to a black list.
    * It's really annoying to develop that but if you had to, please use Redis as your data store
    */
-  route.post('/logout', middlewares.isAuth, (req: Request, res: Response, next: NextFunction) => {
-    const logger = Container.get('logger');
-    logger.debug('Calling Sign-Out endpoint with body: %o', req.body)
-    try {
-      //@TODO AuthService.Logout(req.user) do some clever stuff
-      return res.status(200).end();
-    } catch (e) {
-      logger.error('🔥 error %o', e);
-      return next(e);
-    }
-  });
+  // route.post('/logout', middlewares.isAuth, (req: Request, res: Response, next: NextFunction) => {
+  //   //   const logger = Container.get('logger');
+  //   //   logger.debug('Calling Sign-Out endpoint with body: %o', req.body);
+  //   //   try {
+  //   //     //@TODO AuthService.Logout(req.user) do some clever stuff
+  //   //     return res.status(200).end();
+  //   //   } catch (e) {
+  //   //     logger.error('🔥 error %o', e);
+  //   //     return next(e);
+  //   //   }
+  // });
 };
