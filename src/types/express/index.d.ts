bcf36821fdd461ac0755dcc27cce89ad981d7394
Redeemer40 <victorjonah199@gmail.com>
2020-06-15 18:15:32 +0100

    ...

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,5 +1,7 @@
 import { Document, Model } from 'mongoose';
 import { IUser } from '../../interfaces/IUser';
+import { IVendor } from '../../interfaces/IVendor';
+
 declare global {
   namespace Express {
     export interface Request {
@@ -9,5 +11,6 @@ declare global {
 
   namespace Models {
     export type UserModel = Model<IUser & Document>;
+    export type VendorModel = Model<IVendor & Document>;
   }
 }
