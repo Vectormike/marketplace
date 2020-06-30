546ab4fdb8aa71879c224f9a4611cd0c771c7496
Redeemer40 <victorjonah199@gmail.com>
2020-06-30 17:00:26 +0100

    ...

--- src/api/controllers/auth.ts
+++ src/api/controllers/auth.ts
@@ -1,15 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
 import { Container } from 'typedi';
-import AuthService from '../../services/auth';
+import AuthService from '../../services/user';
 import { IUserInput } from '../../interfaces/IUser';
+import { create } from 'lodash';
 
 const AuthController = {
-  async registerUser(req: Request, res: Response, next: NextFunction) {
+  async createUser(req: Request, res: Response, next: NextFunction) {
     const logger = Container.get('logger');
     logger.debug(`Calling Sign-Up endpoint with body`);
     try {
       const authServiceInstance = Container.get(AuthService);
-      const { user, token } = await authServiceInstance.SignUp(req.body as IUserInput);
+      const { user, token } = await authServiceInstance.createUser(req.body as IUserInput);
       return res.status(201).json({ user, token });
     } catch (error) {
       logger.error(`🔥 error: ${error}`);
