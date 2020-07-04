05e4c880d1ee0e641caad230eadd4218c4f449a2
Redeemer40 <victorjonah199@gmail.com>
2020-07-04 21:41:04 +0100

    add vendor and user controller

--- src/api/controllers/auth.ts
+++ src/api/controllers/auth.ts
@@ -1,17 +1,32 @@
 import { Request, Response, NextFunction } from 'express';
 import { Container } from 'typedi';
-import AuthService from '../../services/user';
+import UserService from '../../services/user';
+import VendorService from '../../services/vendor';
+import AuthService from '../../services/auth';
 import { IUserInput } from '../../interfaces/IUser';
-import { create } from 'lodash';
+import { IVendorInput } from '../../interfaces/IVendor';
 
 const AuthController = {
   async createUser(req: Request, res: Response, next: NextFunction) {
     const logger = Container.get('logger');
     logger.debug(`Calling Sign-Up endpoint with body`);
     try {
-      const authServiceInstance = Container.get(AuthService);
-      const { user, token } = await authServiceInstance.createUser(req.body as IUserInput);
+      const userServiceInstance = Container.get(UserService);
+      const { user, token } = await userServiceInstance.createUser(req.body as IUserInput);
       return res.status(201).json({ user, token });
+    } catch (error) {
+      logger.error(`🔥 error: ${error}`);
+      return next(error);
+    }
+  },
+
+  async createVendor(req: Request, res: Response, next: NextFunction) {
+    const logger = Container.get('logger');
+    logger.debug(`Calling Sign-Up endpoint with body`);
+    try {
+      const vendorServiceInstance = Container.get(VendorService);
+      const { vendor, token } = await vendorServiceInstance.createVendor(req.body as IVendorInput);
+      return res.status(201).json({ vendor, token });
     } catch (error) {
       logger.error(`🔥 error: ${error}`);
       return next(error);
@@ -24,7 +39,21 @@ const AuthController = {
     try {
       const { email, password } = req.body;
       const authServiceInstance = Container.get(AuthService);
-      const { user, token } = await authServiceInstance.SignIn(email, password);
+      const { user, token } = await authServiceInstance.loginUserWithEmailAndPassword(email, password);
+      return res.status(200).json({ user, token });
+    } catch (error) {
+      logger.error(`🔥 error: ${error}`);
+      return next(error);
+    }
+  },
+
+  async loginVendor(req: Request, res: Response, next: NextFunction) {
+    const logger = Container.get('logger');
+    logger.debug(`Calling Sign-In endpoint with body`);
+    try {
+      const { email, password } = req.body;
+      const authServiceInstance = Container.get(AuthService);
+      const { user, token } = await authServiceInstance.loginVendorWithEmailAndPassword(email, password);
       return res.status(200).json({ user, token });
     } catch (error) {
       logger.error(`🔥 error: ${error}`);
