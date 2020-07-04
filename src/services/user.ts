55815ebb3f7f9ee8bc6692bfed8cab826d96795c
Redeemer40 <victorjonah199@gmail.com>
2020-07-04 20:54:51 +0100

    ...

--- src/services/user.ts
+++ src/services/user.ts
@@ -52,14 +52,6 @@ export default class UserService {
   }
 
   // /**
-  //  * Query for users
-  //  * @param {Object} filter - Mongo filter
-  //  * @param {Object} options - Query options
-  //  * @param {string} [options.sortBy] - Sort option in the format: sortField:(desc|asc)
-  //  * @param {number} [options.limit] - Maximum number of results per page (default = 10)
-  //  * @param {number} [options.page] - Current page (default = 1)
-  //  * @returns {Promise<QueryResult>}
-  //  */
   // public async queryUsers(filter, options) {
   //   const users = await this.userModel.paginate(filter, options);
   //   return users;
